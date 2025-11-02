feb1f526a944f504fae5a03320ac6de6bfcdfcf9
Lysa <lysaraz227@gmail.com>
2025-11-02 21:53:54 +0300

    sprint 3

--- src/com/itu/ETU003222/FrontServlet.java
+++ src/com/itu/ETU003222/FrontServlet.java
@@ -1,36 +1,67 @@
-package src.com.itu.ETU003222;
+package com.itu.ETU003222;
 
-import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.*;
+
+import com.itu.ETU003222.Mapping;
+import com.itu.ETU003222.annotation.UrlMapping;
+import com.itu.ETU003222.annotation.AnnotationController;
 
 public class FrontServlet extends HttpServlet {
+
+    private HashMap<String, Mapping> routes = new HashMap<>();
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            scanControllers("controllers");
+        } catch (Exception e) {
+            throw new ServletException("Erreur lors du scan des controllers", e);
+        }
+    }
+
+    private void scanControllers(String packageName) throws Exception {
+        String path = packageName.replace(".", "/");
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL url = loader.getResource(path);
+
+        if (url == null) return;
+
+        File folder = new File(url.toURI());
+        for (File file : folder.listFiles()) {
+            if (file.getName().endsWith(".class")) {
+                String className = packageName + "." + file.getName().replace(".class", "");
+                Class<?> cls = Class.forName(className);
+
+                if (cls.isAnnotationPresent(AnnotationController.class)) {
+                    for (Method m : cls.getDeclaredMethods()) {
+                        if (m.isAnnotationPresent(UrlMapping.class)) {
+                            UrlMapping ann = m.getAnnotation(UrlMapping.class);
+                            routes.put(ann.value(), new Mapping(className, m.getName()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String requestURI = request.getRequestURI();
-        String contextPath = request.getContextPath();
-        String resourcePath = requestURI.substring(contextPath.length());
+        String uri = request.getRequestURI().replace(request.getContextPath(), "");
 
-        try {
-            java.net.URL resource = getServletContext().getResource(resourcePath);
-            if (resource != null) {
-                RequestDispatcher defaultServlet = getServletContext().getNamedDispatcher("default");
-                if (defaultServlet != null) {
-                    defaultServlet.forward(request, response);
-                    return;
-                }
-            }
-        } catch (Exception e) {
-            throw new ServletException("Erreur lors de la vérification de la ressource : " + resourcePath, e);
+        if (routes.containsKey(uri)) {
+            Mapping map = routes.get(uri);
+            response.getWriter().println("Classe trouvée : " + map.className);
+            response.getWriter().println("Méthode trouvée : " + map.methodName);
+            return;
         }
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>FrontServlet</h1>");
-        out.println("<p>URL non trouvée, gérée par le framework : " + resourcePath + "</p>");
-        out.println("</body></html>");
+        response.getWriter().println("URL non trouvée : " + uri);
     }
 }
