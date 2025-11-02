343af29477ccef9f7c3538f946f34706792732c7
Lysa <lysaraz227@gmail.com>
2025-11-02 21:35:28 +0300

    sprint 2 bis

---
+++ src/com/itu/ETU003222/annotation/AnnotationController.java
@@ -0,0 +1,13 @@
+package com.itu.ETU003222.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AnnotationController {
+}
